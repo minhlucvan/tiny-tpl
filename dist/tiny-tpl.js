b8e59d8bdf7807ad68f565f3220a563ddba440e6
minhlucvan <toilatoi123>
2016-12-25 23:10:38 +0700

    add several demos

--- dist/tiny-tpl.js
+++ dist/tiny-tpl.js
@@ -25,8 +25,11 @@
     var CHILD_TEMPLATE_KEY = 'template';
     var DATA_FIELD_KEY = 'field';
 
+    var ANIMATE_SEPARATOR = '|';
+
     var ANIMATE_FADE = 'fade';
     var ANIMATE_FADE_IN = 'fadein';
+    var ANIMATE_EXPAND = 'expand';
 
     /**
      * get deep value of an object
@@ -35,8 +38,8 @@
      * @param selector value selectot ex: field1.field2
      * @returns {*} | null value at location of selector or null of selector not valid
      */
-    function getDeepValue(obj, selector){
-        if(!obj && typeof obj !== 'object' && typeof selector !== 'string') {
+    function getDeepValue(obj, selector) {
+        if (!obj && typeof obj !== 'object' && typeof selector !== 'string') {
             throw new Error("unexpected type passing to function getDeepValue.");
         }
 
@@ -44,7 +47,7 @@
         var value = obj;
 
         fields.forEach(function (field) {
-            if(value && value.hasOwnProperty( field ) ) {
+            if (value && value.hasOwnProperty(field)) {
                 value = value[field];
             } else {
                 value = null;
@@ -60,13 +63,13 @@
      * @param str
      * @returns string tag content
      */
-    function findTag( str ) {
-        var startPost = str.indexOf( OPEN_TAG );
-        var endPost = str.indexOf( CLOSE_TAG, startPost);
+    function findTag(str) {
+        var startPost = str.indexOf(OPEN_TAG);
+        var endPost = str.indexOf(CLOSE_TAG, startPost);
 
         var value = str.substring(startPost + 3, endPost);
 
-        if ( value.length <= 0 || startPost < 0 || endPost < 0) return '';
+        if (value.length <= 0 || startPost < 0 || endPost < 0) return '';
 
         return value;
     }
@@ -82,11 +85,11 @@
         var field = tag;
         var value = getDeepValue(data, field);
 
-        if((typeof data === 'string' || typeof data === 'number') && ( field == STRING_VALUE_TAG) ) {
+        if ((typeof data === 'string' || typeof data === 'number') && ( field == STRING_VALUE_TAG)) {
             value = data;
         }
 
-        if(value == null){
+        if (value == null) {
             value = DEFAULT_FIELD_VALUE;
         }
         return value;
@@ -103,19 +106,19 @@
      */
     function renderSingleData(target, tplStr, data, opt) {
         var $container = $(target);
-        var field = findTag( tplStr );
+        var field = findTag(tplStr);
 
         var result = tplStr;
         var count = 0; // avoid max stacks execution
 
-        while ( field.length > 0 && count < 10000 ){
+        while (field.length > 0 && count < 10000) {
             var value = getTagValue(data, field);
             result = result.replace(OPEN_TAG + field + CLOSE_TAG, value);
-            field = findTag( result );
+            field = findTag(result);
             count++;
         }
 
-        $container.append( result );
+        $container.append(result);
     }
 
     /**
@@ -125,16 +128,24 @@
      * @param data
      * @param opt
      */
-    function doPreScript( element, data, opt ) {
-        if(opt && opt.animate ){
-            var animate = opt.animate;
-            switch ( animate ){
-                case ANIMATE_FADE: {
-                    $(element).fadeOut();
-                    break;
-                }
-                case ANIMATE_FADE_IN: {
-                    $(element).hide();
+    function doPreScript(element, data, opt) {
+        if ( typeof opt.animate === 'string') {
+            opt.hasAnimation = true;
+            opt.animates = opt.animate.split(ANIMATE_SEPARATOR);
+            for (var i = 0; i < opt.animates.length; i++) {
+                var animate = opt.animates[i];
+                switch (animate) {
+                    case ANIMATE_FADE: {
+                        $(element).fadeOut();
+                        break;
+                    }
+                    case ANIMATE_FADE_IN: {
+                        $(element).hide();
+                        break;
+                    }
+                    case ANIMATE_EXPAND: {
+                        opt.oldHeight = $(element).height();
+                    }
                 }
             }
         }
@@ -147,35 +158,46 @@
      * @param data
      * @param opt
      */
-    function doPostScript( element, data, opt ) {
+    function doPostScript(element, data, opt) {
         var $chidConatiner = $(element).find('[data-' + CHILD_TEMPLATE_KEY + ']');
 
-        $chidConatiner.each(function ( ind ) {
-            var template = $(this).data( CHILD_TEMPLATE_KEY );
-            var dataField = $(this).data( DATA_FIELD_KEY );
+        $chidConatiner.each(function (ind) {
+            var template = $(this).data(CHILD_TEMPLATE_KEY);
+            var dataField = $(this).data(DATA_FIELD_KEY);
 
             var nodeData = getDeepValue(data[ind], dataField);
             try {
-                $(this).tinyTpl( nodeData, template);
-            } catch ( e ){
+                $(this).tinyTpl(nodeData, template);
+            } catch (e) {
                 console.error('can not render child template.');
             }
         });
 
-        if(opt && opt.animate ){
-            var animate = opt.animate;
-            switch ( animate ){
-                case ANIMATE_FADE: {
-                    $(element).fadeIn();
-                    break;
-                }
-                case ANIMATE_FADE_IN: {
-                    $(element).fadeIn();
+
+        if ( opt.hasAnimation ) {
+            for (var i = 0; i < opt.animates.length; i++) {
+                var animate = opt.animates[i];
+                switch (animate) {
+                    case ANIMATE_FADE: {
+                        $(element).fadeIn();
+                        break;
+                    }
+                    case ANIMATE_FADE_IN: {
+                        $(element).fadeIn();
+                        break;
+                    }
+                    case ANIMATE_EXPAND: {
+                        var newHeight = $(element).height();
+                        $(element).height(opt.oldHeight);
+                        $(element).animate({height: newHeight}, 'fast', function() {
+                            $(element).height('auto');
+                        });
+                    }
                 }
             }
         }
 
-        if( opt && typeof opt.postScript === 'function'){
+        if (opt && typeof opt.postScript === 'function') {
             opt.postScript(element, data);
         }
     }
@@ -192,46 +214,53 @@
      *    renderMode: single|multi
      * }
      */
-    $.fn.tinyTpl = function(data, tpl ,opt){
-        if ( typeof data !== 'string' && !data ){
+    $.fn.tinyTpl = function (data, tpl, opt) {
+        if (typeof data !== 'string' && !data) {
             return;
         }
 
-        if ( !Array.isArray( data ) ){
-            data = [ data ];
+        if( typeof opt !== 'object' ){
+            opt = {};
         }
 
-        if( typeof tpl === 'object' ){
+        if ( !Array.isArray(data) ) {
+            data = [data];
+        }
+
+        if (typeof tpl === 'object') {
             opt = tpl;
         }
 
-        if( typeof tpl !== 'string' ){
-            tpl = $(this).data( CHILD_TEMPLATE_KEY );
+        if (typeof tpl !== 'string') {
+            tpl = $(this).data(CHILD_TEMPLATE_KEY);
         }
 
         var $tplEl = $(tpl);
-        if( $tplEl.length == 0 ){
+        if ($tplEl.length == 0) {
             throw new Error('can not find template: ' + tpl);
         }
 
         var tplStr = "";
-        if( $tplEl.attr('type') == 'text/template'){
+        if ($tplEl.attr('type') == 'text/template') {
             tplStr = $tplEl.html();
         } else {
             tplStr = $tplEl.get(0).outerHTML;
         }
 
+        var container = this;
+
+        doPreScript(this, data, opt);
+
         $(this).children().not(TPL_PINED_CLASS).remove();
 
-        var container = this;
-        doPreScript(this, data, opt);
-        data.forEach(function ( dat ) {
+        data.forEach(function (dat) {
             renderSingleData(container, tplStr, dat, opt);
         });
 
         doPostScript(this, data, opt);
     };
 
-})( jQuery );
+})(jQuery);
 
 // end template engine
+
